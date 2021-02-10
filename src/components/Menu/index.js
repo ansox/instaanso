5e1430ad2f4496ac890527e83cbf0e9bcc169983
Anso <anderson.floriano@gmail.com>
2021-02-10 07:30:32 -0300

    Aula 04

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../Button';
 import { Logo } from '../theme/Logo';
 import { MenuWrapper } from './styles/MenuWrapper';
 
@@ -25,15 +26,15 @@ export default function Menu() {
       <MenuWrapper.CentralSide>
         {links.map(link => {
           return (
-            <li>
+            <li key={link.url}>
               <a href={link.url}>{link.text}</a>
             </li>
           )
         })}
       </MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>
-        <button>Entrar</button>
-        <button>Cadastrar</button>
+        <Button ghost variant='secondary.main'>Entrar</Button>
+        <Button variant='primary.main'>Cadastrar</Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
   )
