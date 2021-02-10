8fd0b179efce8ab41134284422bc8f1bfa358399
Anso <anderson.floriano@gmail.com>
2021-02-10 09:12:06 -0300

    Aula 05

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Footer from '../src/components/Footer'
 import Menu from '../src/components/Menu'
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        flex: '1',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      }}>
       <Menu></Menu>
+      <Footer />
     </div>
   )
 }
